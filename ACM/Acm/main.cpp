f49b02a5624f3a89c9df5edf5698b5e8a86e1c74
r96922081 <r96922081@gmail.com>
2021-07-04 16:11:54 +0800

    .

---
+++ ACM/Acm/main.cpp
@@ -0,0 +1,10 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+extern void main_11747();
+
+int main()
+{
+    main_11747();
+    return 0;
+}
---
+++ ACM/uva11747.cpp
@@ -0,0 +1,11 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include <iostream>
+
+using namespace std;
+
+void main_11747()
+{
+    cout << "11747" << endl;
+}
