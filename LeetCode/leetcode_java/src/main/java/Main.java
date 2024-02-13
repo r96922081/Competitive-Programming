505f3c4f3cbd23fa1e910aa20f78f1ff72b3383f
r96922081 <r96922081@gmail.com>
2024-02-13 09:37:56 +0800

    .

--- LeetCode/leetcode_java/src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void check(boolean b) {
-        if (!b)
-            throw new Error("check failed!");
-    }
-    public static void main(String[] args) {
-    }
-}
