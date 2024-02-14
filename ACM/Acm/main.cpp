08f0884ef9fd3b162a22d5688c2c95a22a95a181
r96922081 <r96922081@gmail.com>
2024-02-14 18:41:10 +0800

    .

--- ACM/Acm/main.cpp
+++ ACM/Acm/main.cpp
@@ -1,10 +1,23 @@
 #include "stdio.h"
 #include "stdlib.h"
+#include <iostream>
+#include <fstream>
+#include <streambuf>
 
-extern void main_11747();
+using namespace std;
+
+extern int main103();
 
 int main()
 {
-    main_11747();
+    ifstream in("D:\\Github\\Competitive-Programming\\ACM\\_IO\\uva103_input.txt");
+    streambuf* cinbuf = std::cin.rdbuf();
+    cin.rdbuf(in.rdbuf());
+
+    ofstream out("uva103_my_output.txt");
+    streambuf* coutbuf = std::cout.rdbuf(); // Save old cout buffer
+    cout.rdbuf(out.rdbuf());
+
+    main103();
     return 0;
 }
