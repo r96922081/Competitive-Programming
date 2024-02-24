2f2997721b33ac463969cc34b38c94046916bff3
r96922081 <r96922081@gmail.com>
2024-02-24 21:43:06 +0800

    .

---
+++ LeetCode/python/P45.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        return -1
+    
+
+s = Solution()
+
+print(s.jump([1,2,1,1,1]))
+
+print(s.jump([2,3,1,1,4]))
+
+print(s.jump([2,3,0,1,4]))
+
+print(s.jump([0]))
