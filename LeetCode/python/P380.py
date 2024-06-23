b65bc2ea0f68583e2e726775c1bf2cd0e0f3d965
r96922081 <r96922081@gmail.com>
2024-06-23 14:27:13 +0800

    .

--- LeetCode/python/P380.py
+++ LeetCode/python/P380.py
@@ -1,101 +1,51 @@
-from typing import List
 import random
 
-class doubly_linked_list_node:
-    def __init__(self, data):
-        self.data = data
-        self.prev = None
-        self.next = None
-
-class doubly_linked_list:
-    def __init__(self):
-        self.head = None
-        self.tail = None
-
-    def append(self, data):
-        new_node = doubly_linked_list_node(data)
-        if not self.head:
-            self.head = new_node
-            self.tail = new_node
-        else:
-            new_node.prev = self.tail
-            self.tail.next = new_node
-            self.tail = new_node
-
-    def prepend(self, data):
-        new_node = doubly_linked_list_node(data)
-        if not self.head:
-            self.head = new_node
-            self.tail = new_node
-        else:
-            new_node.next = self.head
-            self.head.prev = new_node
-            self.head = new_node
-
-    def delete_by_node(self, node: doubly_linked_list_node):
-        if node == self.head:
-            self.head = node.next
-        if node == self.tail:
-            self.tail = node.prev
-        if node.prev:
-            node.prev.next = node.next
-        if node.next:
-            node.next.prev = node.prev
-
-    def delete_by_data(self, data):
-        n = self.find(data)
-        if n:
-            self.delete_by_node(n)
-
-    def find(self, data):
-        current = self.head
-        while current:
-            if current.data == data:
-                return current
-            current = current.next
-        return None
-
-    def print_list(self):
-        current = self.head
-        while current:
-            print(current.data, end=" ")
-            current = current.next
-        print()
-
-
 class RandomizedSet:
-
-
     def __init__(self):
-        # prime that not close to power of 2
-        self.key = 6091
-        self.bucket = [doubly_linked_list()] * self.key
-        self.non_empty_list = doubly_linked_list()
-        print('hi')
-        
+        self.valToPosDict = {}
+        self.posToValDict = {}
+        self.vector = []
 
     def insert(self, val: int) -> bool:
-        list = self.bucket[val % 6091]
-        if list.head == None:
-            if random.randint(0, 1):
-                self.non_empty_list.append(list)
-            else:
-                self.non_empty_list.prepend(list)
-        if random.randint(0, 1):
-            list.append(val)
-        else:
-            list.prepend(val)
+        if val in self.valToPosDict:
+            return False
+        self.valToPosDict[val] = len(self.vector)
+        self.posToValDict[len(self.vector)] = val
+        self.vector.append(val)
+        return True
+        
 
     def remove(self, val: int) -> bool:
-        list = self.bucket[val % 6091]
-        list.delete_by_data(val)
-        
+        if val not in self.valToPosDict:
+            return False
+
+        pos = self.valToPosDict[val]
+        del self.valToPosDict[val]   
+        del self.posToValDict[pos]
+
+        lastPos = len(self.vector) - 1
+        if pos != lastPos:
+            lastVal = self.vector[lastPos]
+            del self.valToPosDict[lastVal]
+            del self.posToValDict[lastPos]
+            self.vector[pos] = lastVal
+            self.valToPosDict[lastVal] = pos
+            self.posToValDict[pos] = lastVal
+
+        self.vector.pop()
+        return True  
 
     def getRandom(self) -> int:
-        pass
+        return self.vector[random.randrange(0, len(self.vector))]
         
 
+
+# Your RandomizedSet object will be instantiated and called as such:
 obj = RandomizedSet()
-param_1 = obj.insert(1)
-param_3 = obj.getRandom()
-param_2 = obj.remove(1)
+p = obj.remove(0)
+p = obj.remove(0)
+p = obj.insert(0)
+p = obj.getRandom()
+p = obj.remove(0)
+p = obj.insert(0)
+pass
